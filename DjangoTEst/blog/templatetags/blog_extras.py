820e4e7b42b0def4a6b06159988ac24713620105
alexis-valois <alexis@ugotspotted.com>
2014-01-04 16:04:47 -0500

    AJout Costum Tag Random

--- DjangoTEst/blog/templatetags/blog_extras.py
+++ DjangoTEst/blog/templatetags/blog_extras.py
@@ -1,6 +1,7 @@
 #-*- coding:utf-8 -*-
 from django import template
 from django.utils.html import escape
+from random import randint
 
 register = template.Library()
 
@@ -50,3 +51,38 @@ def smart_truncate(texte, nb_caracteres=20):
            texte = texte[0:-1]
 
     return texte + '...'
+
+
+@register.tag
+def random(parser, token):
+    """ Tag générant un nombre aléatoire, entre les bornes données en arguments """
+    #Séparation des paramètres contenus dans l'objet token
+    #Le premier élément du token est toujours le nom du tag en cours
+    try:
+           nom_tag, begin, end = token.split_contents()
+    except ValueError:
+           msg = u'Le tag %s doit prendre exactement deux arguments.' % token.split_contents()[0]
+           raise template.TemplateSyntaxError(msg)
+
+    #Nous vérifions ensuite que nos deux paramètres sont bien des entiers
+    try:
+           begin, end = int(begin), int(end)
+    except ValueError:
+           msg = u'Les arguments du tag %s sont obligatoirement des entiers.' % nom_tag
+           raise template.TemplateSyntaxError(msg)
+
+    #Nous vérifions si le premier est inférieur au second
+    if begin > end:
+           msg = u'L\'argument "begin" doit obligatoirement être inférieur à l\'argument "end" dans le tag %s.' % nom_tag
+           raise template.TemplateSyntaxError(msg)
+
+    return RandomNode(begin, end)
+
+
+class RandomNode(template.Node):
+    def __init__(self, begin, end):
+           self.begin = begin
+           self.end = end
+
+    def render(self, context):
+           return str(randint(self.begin, self.end))
