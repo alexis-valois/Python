d96c7aa47267e62bf8b2b664595855d97f5c48af
alexis-valois <alexis@ugotspotted.com>
2014-01-13 22:42:33 -0500

    ii18n

--- DjangoTEst/DjangoTEst/urls.py
+++ DjangoTEst/DjangoTEst/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^m/', include('mini_url.urls')),
+    (r'^i18n/', include('django.conf.urls.i18n'))
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
